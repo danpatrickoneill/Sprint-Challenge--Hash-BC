7f754b9a70631fa392117667b8dc1c22b0e04a80
danpatrickoneill <dan47573@gmail.com>
2019-12-06 11:50:15 -0500

    hashtable ex1 passing most tests; still work to do

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,8 +12,39 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i, weight in enumerate(weights):
+        print(weight, i)
+        hash_table_insert(ht, weight, i)
+    for weight in weights:
+        if hash_table_retrieve(ht, limit - weight):
+            weight_one = hash_table_retrieve(ht, weight)
+            weight_two = hash_table_retrieve(ht, limit - weight)
+            for item in ht.storage:
+                if item:
+                    print(item.key, ': ', item.value)
+            print((weight_one, weight_two))
+            if weight_one >= weight_two:
+                return (weight_one, weight_two)
+            else:
+                return (weight_two, weight_one)
 
     return None
+
+    # for i, weight in enumerate(weights):
+    #     print(weight, i)
+    #     hash_table_insert(ht, weight, i)
+    # for weight in weights:
+    #     if hash_table_retrieve(ht, limit - weight):
+    #         weight_one = hash_table_retrieve(ht, weight)
+    #         weight_two = hash_table_retrieve(ht, limit - weight)
+    #         for item in ht.storage:
+    #             if item:
+    #                 print(item.key, ': ', item.value)
+    #         print((weight_one, weight_two))
+    #         if weight_one <= weight_two:
+    #             return (weight_one, weight_two)
+    #         else:
+    #             return (weight_two, weight_one)
 
 
 def print_answer(answer):
